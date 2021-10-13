ad0695af82dc7b21691ca33967723fdb4bc0fb6b
asfaqahmed <asfaqahmed356@gmail.com>
2021-10-13 23:08:25 +0530

    'changed'

---
+++ django-ecommerce/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\mohammed fajith\pycharmprojects\django-ecommerce\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
